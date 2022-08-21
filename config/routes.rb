41f18bad7058a0757bd50a86170b0012f626de79
ravi <ravi.shankar@jarvis.consulting>
2022-08-21 21:22:58 +0530

    4thcommit article_model complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles do
+    # get "/articles", to: "articles#index"
+  end
+
 end
 
